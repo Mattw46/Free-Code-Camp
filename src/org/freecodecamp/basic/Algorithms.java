058768cc34e2833d0ba183358770c4ea58c20aaf
Mattw46 <mattw76@live.com.au>
2018-05-03 04:21:01 +0000

    Basic Algorithms

---
+++ src/org/freecodecamp/basic/Algorithms.java
@@ -0,0 +1,8 @@
+package org.freecodecamp.basic;
+
+public class Algorithms {
+    
+    public String reverseString(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+}
---
+++ src/org/freecodecamp/basic/Main.java
@@ -0,0 +1,18 @@
+package org.freecodecamp.basic;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+	    Algorithms algs = new Algorithms();
+	    
+	    testReverseString(algs);
+	    
+    }
+    
+    static void testReverseString(Algorithms algs) {
+        System.out.println("Testing Reverse String");
+	    
+	    System.out.println("Reverse String Passed");   
+    }
+}
