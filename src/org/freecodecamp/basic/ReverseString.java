ea2ea8f96cf931b4c5f0f22611cbd35deaa2636b
Mattw46 <mattw76@live.com.au>
2018-05-07 00:32:22 +0000

    Commented out failing assertion, only used to test exceptions

--- src/org/freecodecamp/basic/ReverseString.java
+++ src/org/freecodecamp/basic/ReverseString.java
@@ -10,7 +10,7 @@ public class ReverseString {
         System.out.println("Testing Reverse String");
 	    assert reverseString("Hello") instanceof String;
         assert reverseString("Hello").equals("olleH");
-        assert reverseString("Hello").equals("elloh");
+        //assert reverseString("Hello").equals("elloh");
         assert reverseString("Howdy").equals("ydwoH");
         assert reverseString("Greetings from Earth").equals("htraE morf sgniteerG");
 	    System.out.println("Reverse String Passed");   
