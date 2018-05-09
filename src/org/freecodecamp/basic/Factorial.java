0addf1f96730fc09d50ff60fd9009a5f911e7d6f
Mattw46 <mattw76@live.com.au>
2018-05-09 06:05:54 +0000

    Implemented factorialize and test successfully with passing and fail tests

--- src/org/freecodecamp/basic/Factorial.java
+++ src/org/freecodecamp/basic/Factorial.java
@@ -2,14 +2,21 @@ package org.freecodecamp.basic;
 
 public class Factorial {
     
-    public int factorialize() {
-        return 0;
+    public int factorialize(int num) {
+        int factorial = 1;
+        int counter = 2;
+        while (counter <= num) {
+            factorial *= counter;
+            counter++;
+        }
+        return factorial;
     }
     
     public void testFactorialize() {
         System.out.println("Testing Factorialize");
         // return int
-        //assert Integer.class.isInstance(algs.factorialize(5));
+        assert Integer.class.isInstance(factorialize(5));
+        //assert Float.class.isInstance(factorialize(5));
         // 5 return 120
         // 10 return 3628800
         // 20 return 2432902008176640000
