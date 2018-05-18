540712b5c2175c25fd166a769cedd6913e69d597
Mattw46 <mattw76@live.com.au>
2018-05-18 02:07:00 +0000

    changed factorial to use long to pass test on large ints

--- src/org/freecodecamp/basic/Factorial.java
+++ src/org/freecodecamp/basic/Factorial.java
@@ -2,9 +2,9 @@ package org.freecodecamp.basic;
 
 public class Factorial {
     
-    public int factorialize(int num) {
-        int factorial = 1;
-        int counter = 2;
+    public long factorialize(long num) {
+        long factorial = 1;
+        long counter = 2;
         while (counter <= num) {
             factorial *= counter;
             counter++;
@@ -13,17 +13,17 @@ public class Factorial {
     }
     
     public void testFactorialize()  throws AssertionError {
-        /* unable to apply test 20 return 2432902008176640000
-         * int type will not hold this value */
         
         System.out.println("Testing Factorialize");
         // return int
-        assert Integer.class.isInstance(factorialize(5));
+        assert Long.class.isInstance(factorialize(5));
         //assert Float.class.isInstance(factorialize(5));
         // 5 return 120
         assert factorialize(5) == 120;
         // 10 return 3628800
         assert factorialize(10) == 3628800;
+        // 20 return 2432902008176640000
+        assert factorialize(20) == 2432902008176640000L;
         // 0 return 1
         assert factorialize(0) == 1;
         System.out.println("Factorialize passed");
