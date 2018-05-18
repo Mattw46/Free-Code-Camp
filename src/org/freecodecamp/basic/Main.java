f4e257f23aba6363f2668841e588054f4e756dfa
Mattw46 <mattw76@live.com.au>
2018-05-18 02:12:22 +0000

    cleaned up main

--- src/org/freecodecamp/basic/Main.java
+++ src/org/freecodecamp/basic/Main.java
@@ -14,14 +14,4 @@ public class Main {
 
     }
     
-     static void testFactorialize() {
-        System.out.println("Testing Factorialize");
-        // return int
-        //assert Integer.class.isInstance(algs.factorialize(5));
-        // 5 return 120
-        // 10 return 3628800
-        // 20 return 2432902008176640000
-        // 0 return 1
-        System.out.println("Factorialize passed");
-    }
 }
